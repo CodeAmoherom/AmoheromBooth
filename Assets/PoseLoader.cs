c7c7be0b197345e44533cdafd9749ad9ca4a37af
Pasan Anuththara <pasananuththara19@gmail.com>
2024-10-23 23:44:28 +0530

    Pose Thumbnail Generator Working

---
+++ Assets/PoseLoader.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoseLoader : MonoBehaviour
+{
+    public GameObject PoseButtonContainer;
+    // Start is called before the first frame
+    void Start()
+    {
+        StartCoroutine(CaptureAllPoses());
+    }
+
+    private IEnumerator CaptureAllPoses()
+    {
+        // Loop through each button in the container
+        foreach (Transform buttonTransform in PoseButtonContainer.transform)
+        {
+            // Get the PoseThumbnailGenerator component attached to the button
+            PoseThumbnailGenerator poseThumbnailGenerator = buttonTransform.GetComponent<PoseThumbnailGenerator>();
+
+            if (poseThumbnailGenerator != null)
+            {
+                // Capture the thumbnail for this pose
+                poseThumbnailGenerator.CaptureThumbnail();
+                print($"Capturing POSE: {poseThumbnailGenerator.poseName}");
+
+                // Wait for the thumbnail to be captured before moving to the next (based on its delay)
+                yield return new WaitForSeconds(poseThumbnailGenerator.DelaySeconds);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
